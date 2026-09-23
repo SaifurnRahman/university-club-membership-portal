b41a7d4453fc621d1b783fd2fad5ba7ef79ed036
Saifur Rahman <saifurrahman24to7@gmail.com>
2026-09-23 16:02:12 +0600

    setup the environment

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+        
+        <p className='text-2xl'>university
+        </p>
+     
+    </>
+  )
+}
+
+export default App
